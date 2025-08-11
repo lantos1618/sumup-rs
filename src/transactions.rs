7f729f2b10701940edd676d279bec648d27a142c
Dominik George <nik@naturalnet.de>
2025-08-11 16:44:53 +0200

    Add retrieve_transaction_by_client_transaction_id

--- src/transactions.rs
+++ src/transactions.rs
@@ -76,6 +76,36 @@ impl SumUpClient {
         }
     }
 
+    /// Retrieves the full details of an identified transaction by client
+    /// transaction id, e.g. as returned by a CreateReaderCheckout request.
+    /// Uses the modern v2.1 endpoint.
+    ///
+    /// # Arguments
+    /// * `merchant_code` - The merchant's unique code.
+    /// * `client_transaction_id` - The transaction's unique ID.
+    pub async fn retrieve_transaction_by_client_transaction_id(
+        &self,
+        merchant_code: &str,
+        client_transaction_id: &str,
+    ) -> Result<Transaction> {
+        let mut url = self.build_url(&format!("/v2.1/merchants/{}/transactions", merchant_code))?;
+        url.query_pairs_mut().append_pair("client_transaction_id", client_transaction_id);
+
+        let response = self
+            .http_client
+            .get(url)
+            .bearer_auth(&self.api_key)
+            .send()
+            .await?;
+
+        if response.status().is_success() {
+            let transaction = response.json::<Transaction>().await?;
+            Ok(transaction)
+        } else {
+            self.handle_error(response).await
+        }
+    }
+
     /// Refunds a transaction.
     ///
     /// # Arguments
